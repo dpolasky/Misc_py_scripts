9d43ee72110956d3b77ebb43914ce1dbfe5c511c
dpolasky <dpolasky@umich.edu>
2019-07-12 18:32:05 -0400

    add oxonium finder

--- ByonicYionFinder.py
+++ ByonicYionFinder.py
@@ -57,7 +57,53 @@ def parse_y_string(y_string):
     return sugar_id, mz
 
 
-def save_output_to_file(dict_y_ions, output_path):
+def get_all_oxonium_ions(file_list):
+    """
+
+    :param file_list:
+    :return:
+    """
+    dict_ox = {}
+    for current_file in file_list:
+        ox_ids, ox_masses = parse_oxonium_file(current_file)
+        for index in range(len(ox_ids)):
+            if ox_ids[index] in dict_ox.keys():
+                dict_ox[ox_ids[index]][1] += 1
+            else:
+                dict_ox[ox_ids[index]] = [ox_masses[index], 1]
+    return dict_ox
+
+
+def parse_oxonium_file(oxfile):
+    """
+    same as Y ion, but for oxonium. Use observed match/theo match combination to determine where they start
+    :param ox_string: string
+    :return:
+    """
+    ox_ids, ox_masses = [], []
+    with open(oxfile, 'r') as readfile:
+        obs_flag = False
+        ox_flag = False
+        for line in list(readfile):
+            if line.startswith('Observed match'):
+                obs_flag = True
+                continue
+            if obs_flag:
+                if line.startswith('Theo'):
+                    # keep waiting, not to the line we want yet
+                    obs_flag = False
+                else:
+                    # we've reached the oxonium ion section
+                    ox_flag = True
+            if ox_flag:
+                splits = line.rstrip('\n').split(',')
+                ox_ids.append(splits[0].strip())
+                mass_splits = splits[1].strip().split('=')
+                ox_masses.append(float(mass_splits[1].strip()))
+    return ox_ids, ox_masses
+
+
+def save_output_to_file(dict_y_ions, output_path, mass_mode=False):
     """
     Save the dictionary of unique sugars to text file
     :param dict_y_ions: dict of key=sugar ID, value=mass, count
@@ -67,7 +113,10 @@ def save_output_to_file(dict_y_ions, output_path):
     with open(output_path, 'w') as outfile:
         outfile.write('Y Glycan,count\n')
         for key, value in dict_y_ions.items():
-            outfile.write('{},{}\n'.format(key, value))
+            if mass_mode:
+                outfile.write('{},{},{}\n'.format(key, value[0], value[1]))
+            else:
+                outfile.write('{},{}\n'.format(key, value))
 
 
 if __name__ == '__main__':
@@ -76,5 +125,8 @@ if __name__ == '__main__':
 
     folder = filedialog.askdirectory()
     files = [os.path.join(folder, x) for x in os.listdir(folder) if x.endswith('.csv')]
-    output_dict = getAllYionsByonic(files)
-    save_output_to_file(output_dict, os.path.join(folder, 'output.csv'))
+
+    # output_dict = getAllYionsByonic(files)
+    output_dict = get_all_oxonium_ions(files)
+
+    save_output_to_file(output_dict, os.path.join(folder, 'output.csv'), mass_mode=True)
