725796629893ae1a6389ccb63bfa93cef138df44
dpolasky <dpolasky@umich.edu>
2019-09-16 16:04:17 -0400

    Add non-deisotope run method to MSConvert wrapper and option to do both

--- MSConvertWrapper.py
+++ MSConvertWrapper.py
@@ -9,18 +9,30 @@ import subprocess
 
 tool_path = r"C:\Program Files\ProteoWizard\ProteoWizard 3.0.19194.9338c77b2\msconvert.exe"
 out_dir = ''
+DEISOTOPE = True
+RUN_BOTH = True
 
 
-def format_commands(filename):
+def format_commands(filename, deisotope):
     """
     Generate a formatted string for running MSConvert
     :param filename:
+    :param deisotope: boolean
     :return: string
     """
-    cmd_str = '{} {} --mzML -z --64 -o {}'.format(tool_path, filename, out_dir)
+    # create output directory
+    if deisotope:
+        output_dir = os.path.join(os.path.dirname(filename), 'DeIso_output')
+    else:
+        output_dir = os.path.dirname(filename)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    cmd_str = '{} {} --mzML -z --64 -o {}'.format(tool_path, filename, output_dir)
     cmd_str += ' --filter "peakPicking true 1-"'
-    cmd_str += ' --filter "MS2Deisotope Poisson minCharge=1 maxCharge=5"'
-    # cmd_str += ' --filter "MS2Deisotope hi_res"'
+    if deisotope:
+        cmd_str += ' --filter "MS2Deisotope Poisson minCharge=1 maxCharge=6"'
+        # cmd_str += ' --filter "MS2Deisotope hi_res"'
 
     print(cmd_str)
     return cmd_str
@@ -32,10 +44,13 @@ if __name__ == '__main__':
 
     files = filedialog.askopenfilenames(filetypes=[('Raw', '.raw')])
     files = [os.path.join(os.path.dirname(x), x) for x in files]
-    out_dir = os.path.join(os.path.dirname(files[0]), 'DeIso_output')
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
 
     for file in files:
-        cmd = format_commands(file)
-        subprocess.run(cmd)
+        if RUN_BOTH:
+            cmd = format_commands(file, deisotope=True)
+            subprocess.run(cmd)
+            cmd = format_commands(file, deisotope=False)
+            subprocess.run(cmd)
+        else:
+            cmd = format_commands(file, deisotope=DEISOTOPE)
+            subprocess.run(cmd)
