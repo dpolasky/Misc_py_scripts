db5bfe39c00408c77836a98c608a0e091b0ff68c
dpolasky <dpolasky@umich.edu>
2019-12-11 12:09:22 -0500

    Chunk output writing for improved speed

--- Digest_to_Fasta.py
+++ Digest_to_Fasta.py
@@ -7,6 +7,7 @@ import tkinter
 from tkinter import filedialog
 import os
 from pyteomics import fasta, parser
+import time
 
 
 def do_digest(seq, res_to_cut_list, max_missed_cleavages, cut_cterm=True):
@@ -68,7 +69,7 @@ def digest_custom_manual(fasta_file, res_to_cut, max_missed_cleavages, min_lengt
     index = 0
     for protein_descript, protein_seq in fasta.read(fasta_file):
         if index % 10000 == 0:
-            print('parsed seq {}...'.format(index))
+            print('digested seq {}...'.format(index))
         index += 1
         peptides = do_digest(protein_seq, res_to_cut, max_missed_cleavages, c_term)
         peptides = [x for x in peptides if min_length <= len(x) <= max_length]
@@ -93,7 +94,7 @@ def digest_fasta_trypsin(fasta_file, min_length, max_length):
     index = 0
     for protein_descript, protein_seq in fasta.read(fasta_file):
         if index % 10000 == 0:
-            print('parsed seq {}...'.format(index))
+            print('digested seq {}...'.format(index))
         index += 1
         # digest this protein using parser.cleave and filter by min and max length
         new_peptides = parser.cleave(protein_seq, parser.expasy_rules['trypsin'], missed_cleavages=2, min_length=min_length)
@@ -119,7 +120,7 @@ def print_counts(output_fasta_dict):
     :rtype:
     """
     print('total peptides = {}'.format(len(output_fasta_dict.keys())))
-    print('total unique peptides = {}'.format(len(set(output_fasta_dict.keys()))))
+    # print('total unique peptides = {}'.format(len(set(output_fasta_dict.keys()))))
     counts = {x: 0 for x in range(5)}
     counts[5] = 0
     for peptide, prot_list in output_fasta_dict.items():
@@ -132,7 +133,7 @@ def print_counts(output_fasta_dict):
 
 def write_fasta(fasta_dict, output_path):
     """
-    manually write output because pyteomics is super slow...
+    manually write output because pyteomics is super slow. Chunked to try to improve speed
     :param fasta_dict: dict of seq: description
     :type fasta_dict: dict
     :param output_path: full path
@@ -142,11 +143,18 @@ def write_fasta(fasta_dict, output_path):
     """
     index = 0
     with open(output_path, 'w') as outfile:
+        current_chunk = []
+        current_time = time.time()
         for seq, description in fasta_dict.items():
-            if index % 100000 == 0:
-                print('printed seq {} of {}'.format(index, len(fasta_dict.keys())))
+            if index % 1000000 == 0:
+                print('printed seq {} of {} in {:.1f} s'.format(index, len(fasta_dict.keys()), time.time() - current_time))
+                current_time = time.time()
+            if index % 10000 == 0:
+                outfile.write(''.join(current_chunk))
+                current_chunk = []
+            current_chunk.append('>{}\n{}\n'.format(description[0], seq))
             index += 1
-            outfile.write('>{}\n{}\n'.format(description[0], seq))
+            # outfile.write('>{}\n{}\n'.format(description[0], seq))
 
 
 if __name__ == '__main__':
@@ -158,7 +166,7 @@ if __name__ == '__main__':
 
     for file in files:
         # fasta_info = digest_fasta_trypsin(file, min_length=7, max_length=60)
-        fasta_info = digest_custom_manual(file, res_to_cut=['S', 'T'], max_missed_cleavages=7, min_length=7, max_length=60, c_term=False)
+        fasta_info = digest_custom_manual(file, res_to_cut=['S', 'T'], max_missed_cleavages=20, min_length=7, max_length=1000, c_term=False)
         new_filename = os.path.splitext(file)[0] + '_digest.fasta'
         print('writing output')
         # fasta.write(fasta_info, output=new_filename)
