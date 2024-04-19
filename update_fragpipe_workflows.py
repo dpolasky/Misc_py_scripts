fad8bff46a83453ad6a2218da355fbfbe1b782ee
Dan Polasky <dpolasky@umich.edu>
2024-04-19 14:35:07 -0400

    new local paths

--- update_fragpipe_workflows.py
+++ update_fragpipe_workflows.py
@@ -10,9 +10,9 @@ import os
 import shutil
 
 # WORKFLOW_DIR = r"C:\Users\dpolasky\GitRepositories\FragPipe\FragPipe\MSFragger-GUI\build\classes\java\workflows"
-WORKFLOW_DIR = r"C:\Users\dpolasky\GitRepositories\FragPipe\FragPipe\MSFragger-GUI\build\install\fragpipe\workflows"
-REPO_DIRS = [r"C:\Users\dpolasky\GitRepositories\FragPipe\FragPipe\MSFragger-GUI\resources\workflows",
-             r"C:\Users\dpolasky\GitRepositories\FragPipe\FragPipe\MSFragger-GUI\workflows"]
+WORKFLOW_DIR = r"C:\Users\dpolasky\Repositories\FragPipe\MSFragger-GUI\build\install\fragpipe\workflows"
+REPO_DIRS = [r"C:\Users\dpolasky\Repositories\FragPipe\MSFragger-GUI\resources\workflows",
+             r"C:\Users\dpolasky\Repositories\FragPipe\MSFragger-GUI\workflows"]
 
 REQUIRED_STRINGS = ['glyco', 'Labile', 'FPOP']
 EDIT_EXISTING_DEFAULTS = False   # if true, edit files with no spaces as well as with spaces
