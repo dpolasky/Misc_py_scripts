8c9f480c8d1624593d165288e12c8cd18d561dbe
Dan Polasky <dpolasky@umich.edu>
2024-05-20 15:46:49 -0400

    add msfragger upload option

--- Copy_FragPipe_For_Testing.py
+++ Copy_FragPipe_For_Testing.py
@@ -8,10 +8,15 @@ import shutil
 
 # FRAGPIPE_REPO = r"C:\Users\dpolasky\GitRepositories\FragPipe\FragPipe\MSFragger-GUI\build\install\fragpipe"
 FRAGPIPE_REPO = r"C:\Users\dpolasky\Repositories\FragPipe\MSFragger-GUI\build\install\fragpipe"
-TEST_DIR = r"Z:\dpolasky\projects\_BuildTests\tools"
-SKIP_VERSION = False
-# TEST_DIR = r"Z:\dpolasky\tools\_FragPipes\a_current"
-# SKIP_VERSION = True
+MSFRAGGER_DIR = r"C:\Users\dpolasky\Repositories\MSFragger\target"
+
+# TEST_DIR = r"Z:\dpolasky\projects\_BuildTests\tools"
+# SKIP_VERSION = False
+TEST_DIR = r"Z:\dpolasky\tools\_FragPipes\a_current"
+SKIP_VERSION = True
+# COPY_MSF_TOO = True
+COPY_MSF_TOO = False
+TOOLS_DIR = r"Z:\dpolasky\tools"
 
 
 def get_version(fragpipe_path):
@@ -40,6 +45,13 @@ def main():
     :return:
     :rtype:
     """
+    if COPY_MSF_TOO:
+        msf_files = [os.path.join(MSFRAGGER_DIR, x) for x in os.listdir(MSFRAGGER_DIR) if 'msfragger' in x and 'proguard' not in x and 'original' not in x]
+        msf_src = pathlib.Path(msf_files[0])
+        msf_dest = pathlib.Path(TOOLS_DIR, os.path.basename(msf_src))
+        print('copying {} to {}'.format(msf_src, msf_dest))
+        shutil.copy(msf_src, msf_dest)
+
     version = get_version(FRAGPIPE_REPO)
     if SKIP_VERSION:
         copy_path = pathlib.Path(TEST_DIR)
